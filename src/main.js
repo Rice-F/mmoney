13de71aeae39b802fbd67481b78127dd27649141
Rice-F <2604211051@qq.com>
2018-09-15 00:31:08 +0800

    init project structure

--- src/main.js
+++ src/main.js
@@ -3,8 +3,20 @@
 import Vue from 'vue'
 import App from './App'
 import router from './router'
+// 移动端300ms点击优化
+import fastClick from 'fastclick'
+// 轮播插件
+import VueAwesomeSwiper from 'vue-awesome-swiper'
+import 'swiper/dist/css/swiper.css'
+import 'styles/reset.css'
+// 解决2、3倍屏的1px边框问题
+import 'styles/border.css'
+// 解决不支持promise的手机会出现白屏的问题
+import 'babel-polyfill'
 
 Vue.config.productionTip = false
+fastClick.attach(document.body)
+Vue.use(VueAwesomeSwiper)
 
 /* eslint-disable no-new */
 new Vue({
