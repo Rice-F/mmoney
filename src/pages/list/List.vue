90a46d0a56ae2d440a19cbf39d0c5e3e7efc4ef9
Rice-F <2604211051@qq.com>
2018-09-16 17:39:54 +0800

    add search component

--- src/pages/list/List.vue
+++ src/pages/list/List.vue
@@ -1,15 +1,19 @@
 <template>
   <div>
     <list-swiper></list-swiper>
+    <list-search></list-search>
   </div>
 </template>
 
 <script>
 import ListSwiper from './components/Swiper'
+import ListSearch from './components/Search'
+
 export default {
   name: 'List',
   components: {
-    ListSwiper
+    ListSwiper,
+    ListSearch
   }
 }
 </script>
---
+++ src/pages/list/components/Search.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="search">
+    <div class="wrapper">
+      <div class="search-title">
+        <span>文搜</span>
+        <span>图搜</span>
+      </div>
+      <div class="search-content">
+        <div class="search-input">
+          请输入需要查询的贷款产品
+        </div>
+        <button>查网贷</button>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'ListSearch'
+}
+</script>
+
+<style lang="stylus" scoped>
+  .wrapper
+    padding .2rem .4rem
+    background #f7f7f7
+    .search-title
+      margin-bottom .2rem
+      span
+        display inline-block
+        margin-right .2rem
+    .search-content
+      display flex
+      .search-input
+        flex 1
+        height .64rem
+        line-height .64rem
+        padding-left .2rem
+        background #fff
+        color #ccc
+      button
+        min-width 1.4rem
+        text-align center
+        background #0ac19c
+        color #fff
+</style>
